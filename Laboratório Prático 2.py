6d7b6692e3e9303eabb1c34367f377e694798877
Fernnado Henrique Bugalho Ferreira <fernando_bugalho@outlook.com>
2024-11-21 04:35:47 -0300

    Update Laboratório Prático 2.py

--- Laboratório Prático 2.py
+++ Laboratório Prático 2.py
@@ -4,15 +4,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x):
-    
-    return (2 * x + 3)
+x = np.linspace(5, 10, 100)
+y = 2 * x + 3
 
-x = np.linspace(5,10)
-
-plt.figure(figsize = (6,4))
-plt.plot(x, f(x), label = 'y = 2x + 3')
-plt.grid()
+plt.plot(x, y)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Gráfico da Função Linear y = 2x + 3')
+plt.grid(True)
+plt.show()
 
 #Exercício 2: Gráfico de uma Função Quadrática
 #Desenhe o gráfico da função y = x^2 - 4x + 4. Crie uma massa de dados de exemplo usando np.linspace().
